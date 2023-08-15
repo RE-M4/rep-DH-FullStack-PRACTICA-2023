73e9f6475a60b53c674de44d0d702766507e77f6
RE-M4 <evilmlr2018@gmail.com>
2023-08-15 15:04:02 -0300

    Adición de contenido de clases 21 y 22

--- app.js
+++ app.js
@@ -6,10 +6,24 @@ const publicPath = path.resolve(__dirname,"./public")
 
 app.use(express.static(publicPath))
 
-app.listen(3000, () => {
+//Render establece un puerto por defecto, por lo que este bloque debe ser reemplazado
+/*app.listen(3000, () => {
     console.log("Server is running");
+})*/
+
+const port = process.env.port || 3001;
+app.listen(port, () => {
+    console.log("Server corriendo en el port " + port);
 })
 
 app.get('/', function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
+
+app.get('/registro', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/register.html'))
+})
+
+app.get('/ingreso', function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/login.html'))
 })
